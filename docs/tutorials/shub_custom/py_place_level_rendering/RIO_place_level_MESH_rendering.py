SAMPLE places rendering DONE, however as per correct rotation formulation remaining. Right now just experimenting with R values

docs/tutorials/shub_custom/py_place_level_rendering/RIO_place_level_MESH_rendering.py
@@ -45,6 +45,7 @@ from pytorch3d.renderer import (
 from pytorch3d.utils import cameras_from_opencv_projection
 
 from utils import synthesize_img_given_viewpoint, load_pcd_mat, load_view_point, parse_camera_file_RIO, parse_pose_file_RIO
+from output_places_poses_RIO_convex_hull import poses_for_places
 
 
 def RtK_in_torch_format(K, RT, img_size):
@@ -84,7 +85,8 @@ def main(img_ids, save_imgs):
     #added format code, so just
     #integer format
 
-    seq01_XX = ["01", "02"]
+    # seq01_XX = ["01", "02"]
+    seq01_XX = ["01"]
     for seq_id in seq01_XX:
         for num in img_ids:
 
@@ -152,7 +154,7 @@ def main(img_ids, save_imgs):
             )
 
             rendered_images = renderer(mesh)
-            # plt.imshow(images[0, ..., :3].cpu().numpy())
+            # plt.imshow(rendered_images[0, ..., :3].cpu().numpy())
             # plt.show()
             
             given_img = plt.imread(rgb_file)
@@ -167,7 +169,109 @@ def main(img_ids, save_imgs):
                 print(f"img saved to outputs/{seq_id + str(num)+ img_type}.png")
             plt.show()
 
+def main_given_poses(given_poses, save_imgs):
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        torch.cuda.set_device(device)
+    else:
+        device = torch.device("cpu")
+
+    #change file num: 
+    #added format code, so just
+    #integer format
+
+    # seq01_XX = ["01", "02"]
+    seq01_XX = ["01"]
+    for seq_id in seq01_XX:
+        for pose_tf in given_poses: #for num in img_ids
+
+            pose_file_dir = "/scratch/saishubodh/RIO10_data/scene01/seq01/seq01_" + seq_id +"/"
+            camera_file = os.path.join(pose_file_dir, 'camera.yaml')
+
+            camera_file = 'camera.yaml'
+            # pose_file = 'frame-{:06d}.pose.txt'.format(num)
+            # rgb_file = 'frame-{:06d}.color.jpg'.format(num)
+
+            camera_file = os.path.join(pose_file_dir, camera_file)
+            # pose_file = os.path.join(pose_file_dir, pose_file)
+            # rgb_file = os.path.join(pose_file_dir, rgb_file)
+
+            print(camera_file, os.path.isfile(camera_file))
+            # print(pose_file, os.path.isfile(pose_file))
+            # print(rgb_file, os.path.isfile(rgb_file))
+            # img = plt.imread(rgb_file)
+            # plt.imshow(img)
+            # plt.show()
+
+            o3d_param, K, img_size =  parse_camera_file_RIO(camera_file)
+            # o3d_param, RT, RT_ctow = parse_pose_file_RIO(pose_file, o3d_param)
+            RT = pose_tf
+            RT_ctow = np.zeros((4,4))
+            RT_wtoc = RT
+
+            print(f"H & W: {img_size}, \n K:\n{K}, \n tf w to c:\n{RT} \n tf c to w:\n{RT_ctow} ")
+
+            mesh_dir = "/scratch/saishubodh/RIO10_data/scene01/models01/seq01_01/"
+            mesh_obj_file = os.path.join(mesh_dir ,"mesh.obj")
+            print("Testing IO for meshes ...")
+            mesh = load_objs_as_meshes([mesh_obj_file], device=device)
+            print("Mesh loading done !!!")
+
+            texture_image=mesh.textures.maps_padded()
+            # plt.figure(figsize=(7,7))
+            # plt.imshow(texture_image.squeeze().cpu().numpy())
+            # plt.show()
+
+
+            RR, tt, KK, img_size_t = RtK_in_torch_format(K, RT, img_size)
+
+            cameras_pytorch3d = cameras_from_opencv_projection(RR.float(), tt.float(), KK.float(), img_size_t.float())
+            # above line was giving dtype errors, so made everything float..
+            cameras_pytorch3d  = cameras_pytorch3d.to(device)
+            # (img_size_t.float().dtype, RR.dtype)
+            raster_settings = RasterizationSettings(
+                image_size=img_size, 
+                blur_radius=0.0, 
+                faces_per_pixel=1, 
+            )
+
+            #lights = lights_given_position(RT_ctow[0:3, 3], device)
+            lights = lights_given_position(RT_wtoc[0:3, 3], device)
+
+            renderer = MeshRenderer(
+                rasterizer=MeshRasterizer(
+                    cameras=cameras_pytorch3d, 
+                    raster_settings=raster_settings
+                ),
+                shader=SoftPhongShader(
+                    device=device,
+                    cameras=cameras_pytorch3d,
+                    lights=lights
+                )
+            )
+
+            rendered_images = renderer(mesh)
+            plt.imshow(rendered_images[0, ..., :3].cpu().numpy())
+            plt.show()
+            
+            # given_img = plt.imread(rgb_file)
+            # fig, (ax1, ax2) = plt.subplots(1, 2)
+            # ax1.imshow(given_img)
+            # ax2.imshow(rendered_images[0, ..., :3].cpu().numpy())
+
+            #img_type = "_true-pose-ctow"
+            img_type = "_random-pose-wtoc"
+            if save_imgs:
+                plt.savefig("outputs/" + seq_id + "_" + str(num) + img_type + ".png")
+                print(f"img saved to outputs/{seq_id + str(num)+ img_type}.png")
+            plt.show()
+
+
 if __name__=='__main__':
-    img_ids = [131, 1992, 3530, 3622]
+    # img_ids = [131, 1992, 3530, 3622]
+    img_ids = [131]
     save_imgs = False
-    main(img_ids, save_imgs)
+    given_poses = poses_for_places()
+    # print(given_poses)
+    # main(img_ids, save_imgs, given_poses)
+    main_given_poses(given_poses, save_imgs)

docs/tutorials/shub_custom/py_place_level_rendering/output_places_poses_RIO_convex_hull.py
@@ -66,16 +66,18 @@ def o3dglobalframe_from_coords(coords=[0,0,0]):
     coord_mesh = o3d.geometry.TriangleMesh.create_coordinate_frame(origin=[0, 0, 0], size=1)
 
     T = np.eye(4)
-    T[:3, :3] = coord_mesh.get_rotation_matrix_from_xyz((np.pi / 2, 0,  np.pi ))
+    # T[:3, :3] = coord_mesh.get_rotation_matrix_from_xyz((np.pi / 2, 0,  np.pi ))
+    T[:3, :3] = coord_mesh.get_rotation_matrix_from_xyz((np.pi / 2, -np.pi/4,  np.pi ))
     T[0:3,3] = coords
+    coord_mesh.transform(T)
+    # ## mesh_t = copy.deepcopy(coord_mesh).transform(T)
 
     # coord_mesh.translate(coords)
     # coord_mesh.rotate(R, center=coords)
-    mesh_t = copy.deepcopy(coord_mesh).transform(T)
 
     # coord_pcd =  coord_mesh.sample_points_uniformly(number_of_points=500)
     # o3d.visualization.draw_geometries([coord_mesh])
-    return mesh_t, T
+    return coord_mesh, np.linalg.inv(T)
 
 def viz_centroids_and_center(centroids_coordinates, sphere_center_coords, mesh):
     # print(f"{centroids_coordinates.shape}")
@@ -101,7 +103,7 @@ def viz_centroids_and_center(centroids_coordinates, sphere_center_coords, mesh):
 
 def find_final_poses_from_centroids_and_center(centroids_coordinates, sphere_center_coords, mesh):
     # print(centroids_coordinates[0], sphere_center_coords)
-    sample_poses =  np.linspace(centroids_coordinates[0], sphere_center_coords, num=5)
+    sample_poses =  np.linspace(centroids_coordinates[10], sphere_center_coords, num=5)
     # hi =(centroids_coordinates[0]+ sphere_center_coords ) /2
     # hii = (hi + sphere_center_coords)/2
     # hiii = (centroids_coordinates[0] + hi)/2