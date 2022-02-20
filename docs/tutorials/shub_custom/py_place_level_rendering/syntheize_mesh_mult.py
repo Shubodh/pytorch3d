cd20abde9858498b016638a74c87221cbfab23c3
Shubodh ADA g08 <p.saishubodh@gmail.com>
2022-02-20 19:12:48 +0530

    cleaning code: WIP

--- docs/tutorials/shub_custom/py_place_level_rendering/syntheize_mesh_mult.py
+++ docs/tutorials/shub_custom/py_place_level_rendering/syntheize_mesh_mult.py
@@ -50,6 +50,93 @@ from pytorch3d.utils import cameras_from_opencv_projection
 
 # import open3d as o3d
 # %matplotlib inline
+# def redner_py3d_img(img_size, param, dest_file, mesh_dir, device):
+def redner_py3d_img(img_size, param, dest_file, mesh_dir, device = None):
+    K = param.intrinsic.intrinsic_matrix
+    RT = param.extrinsic
+    # print(K, RT)
+
+    mesh_obj_file = os.path.join(mesh_dir, 'mesh.obj')
+    print("Testing IO for meshes ...")
+    if device is not None:
+        mesh = load_objs_as_meshes([mesh_obj_file], device=device)
+    else:
+        mesh = load_objs_as_meshes([mesh_obj_file])
+    print("Mesh loading done !!!")
+    texture_image=mesh.textures.maps_padded()
+    # plt.figure(figsize=(7,7))
+    # plt.imshow(texture_image.squeeze().cpu().numpy())
+    # plt.show()
+    RR, tt, KK, img_size_t = RtK_in_torch_format(K, RT, img_size)
+    # print(RR)
+    cameras_pytorch3d = cameras_from_opencv_projection(RR.float(), tt.float(), KK.float(), img_size_t.float())
+    if device is not None:
+        cameras_pytorch3d  = cameras_pytorch3d.to(device)
+
+    raster_settings = RasterizationSettings(
+                image_size=img_size, 
+                blur_radius=0.0, 
+                faces_per_pixel=1, 
+            )
+    # lights = lights_given_position(RT_wtoc[0:3, 3], device
+    print("TODO2: Change RT variable explicitly to wtoc or ctow")
+    RT = convert_w_t_c(RT)
+    if device is not None:
+        lights = lights_given_position(RT[0:3, 3], device)
+        
+    renderer = MeshRenderer(
+                rasterizer=MeshRasterizer(
+                    cameras=cameras_pytorch3d, 
+                    raster_settings=raster_settings
+                ),
+                shader=SoftPhongShader(
+                    device=device,
+                    cameras=cameras_pytorch3d,
+                    lights=lights
+                )
+            )
+    rendered_images = renderer(mesh)
+    rendered_image = rendered_images[0, ..., :3].cpu().numpy()
+
+    plt.imsave(dest_file, rendered_image)
+
+def convert_w_t_c(RT_ctow):
+    """
+    input RT is transform from camera to world
+    o3d requires world to camera
+    """
+    print("TODO1: INCORRECT CODE: RT_wtoc doesn't have 1 at 3,3 position")
+    RT_wtoc = np.zeros((RT_ctow.shape))
+    RT_wtoc[0:3,0:3] = RT_ctow[0:3,0:3].T
+    RT_wtoc[0:3,3] = - RT_ctow[0:3,0:3].T @ RT_ctow[0:3,3]
+    return RT_wtoc
+
+def lights_given_position(position, device):
+    # lights = PointLights(device=device, location=[[-(1.67-4.2)/2, -(4.05 - 0.38)/2, 1.0]])
+    # lights = PointLights(device=device, location=[[0.0, 0.0, 3.0]])
+    # lights = PointLights(device=device, location=[[1.138, -1.92, 1.54]])
+    # one, two, three = 1.88506202, -1.34736095,  1.11616586 # mesh center
+    one, two, three = position
+    lights = PointLights(device=device, location=[[ one, two, three]])
+    #print(one, two, three)
+    return lights
+
+def RtK_in_torch_format(K, RT, img_size):
+    # fx, fy = K[0,0], K[1,1] # focal length in x and y axes
+    # px, py = K[0,2], K[1,2] # principal points in x and y axes
+    R, t =  RT[0:3,0:3], np.array([RT[0:3,3]]) #  rotation and translation matrix
+
+    RR = torch.from_numpy(R).unsqueeze(0) # dim = (1, 3, 3)
+    KK = torch.from_numpy(K).unsqueeze(0) # dim = (1, 3, 3)
+    # following line transposes matrix
+    # RR = torch.from_numpy(R).permute(1,0).unsqueeze(0) # dim = (1, 3, 3) 
+    tt = torch.from_numpy(t) # dim = (1, 3)
+    # f = torch.tensor((fx, fy), dtype=torch.float32).unsqueeze(0) # dim = (1, 2)
+    # p = torch.tensor((px, py), dtype=torch.float32).unsqueeze(0) # dim = (1, 2)
+    img_size_t = torch.tensor(img_size).unsqueeze(0) # (width, height) of the image
+
+    return RR, tt, KK, img_size_t
+
 
 class camera_params(object):
     """docstring for camera_params"""
@@ -82,8 +169,6 @@ class camera_params(object):
         print("camera model:" ,model)
         print("img size", img_size)
         print("K", K)
-
-        
 
 def convex_hull(mesh):
 
@@ -143,7 +228,6 @@ def o3dframe_from_coords(RT, color=[1, 0.706, 0], radius = 0.2):
     frame_mesh.transform(RT)
     return frame_mesh
 
-
 def o3dsphere_from_coords(coords, color=[1, 0.706, 0], radius = 0.2):
     sphere_mesh = o3d.geometry.TriangleMesh.create_sphere(radius)
     sphere = copy.deepcopy(sphere_mesh).translate(coords)
@@ -151,10 +235,8 @@ def o3dsphere_from_coords(coords, color=[1, 0.706, 0], radius = 0.2):
 
     return sphere 
 
-
-
-
 def create_rt(lookat,location):
+    print("TODO5: I think Rotation should be transpose of what it is. Check visually.")
     z_axis = np.array(lookat) - np.array(location)
     z = z_axis/np.linalg.norm(z_axis)
     y = np.array([0,0,-1])
@@ -166,100 +248,6 @@ def create_rt(lookat,location):
     RT[0:3,3] = np.array(location)
     RT[3,3] = 1 
     return RT 
-
-def convert_w_t_c(RT_ctow):
-    """
-    input RT is transform from camera to world
-    o3d requires world to camera
-    """
-    RT_wtoc = np.zeros((RT_ctow.shape))
-    RT_wtoc[0:3,0:3] = RT_ctow[0:3,0:3].T
-    RT_wtoc[0:3,3] = - RT_ctow[0:3,0:3].T @ RT_ctow[0:3,3]
-    return RT_wtoc
-
-
-def lights_given_position(position, device):
-    # lights = PointLights(device=device, location=[[-(1.67-4.2)/2, -(4.05 - 0.38)/2, 1.0]])
-    # lights = PointLights(device=device, location=[[0.0, 0.0, 3.0]])
-    # lights = PointLights(device=device, location=[[1.138, -1.92, 1.54]])
-    # one, two, three = 1.88506202, -1.34736095,  1.11616586 # mesh center
-    one, two, three = position
-    lights = PointLights(device=device, location=[[ one, two, three]])
-    #print(one, two, three)
-    return lights
-
-
-
-
-
-
-def RtK_in_torch_format(K, RT, img_size):
-    # fx, fy = K[0,0], K[1,1] # focal length in x and y axes
-    # px, py = K[0,2], K[1,2] # principal points in x and y axes
-    R, t =  RT[0:3,0:3], np.array([RT[0:3,3]]) #  rotation and translation matrix
-
-    RR = torch.from_numpy(R).unsqueeze(0) # dim = (1, 3, 3)
-    KK = torch.from_numpy(K).unsqueeze(0) # dim = (1, 3, 3)
-    # following line transposes matrix
-    # RR = torch.from_numpy(R).permute(1,0).unsqueeze(0) # dim = (1, 3, 3) 
-    tt = torch.from_numpy(t) # dim = (1, 3)
-    # f = torch.tensor((fx, fy), dtype=torch.float32).unsqueeze(0) # dim = (1, 2)
-    # p = torch.tensor((px, py), dtype=torch.float32).unsqueeze(0) # dim = (1, 2)
-    img_size_t = torch.tensor(img_size).unsqueeze(0) # (width, height) of the image
-
-    return RR, tt, KK, img_size_t
-
-
-
-
-# def redner_py3d_img(img_size, param, dest_file, mesh_dir, device):
-def redner_py3d_img(img_size, param, dest_file, mesh_dir, device = None):
-    K = param.intrinsic.intrinsic_matrix
-    RT = param.extrinsic
-    # print(K, RT)
-
-    mesh_obj_file = os.path.join(mesh_dir, 'mesh.obj')
-    print("Testing IO for meshes ...")
-    if device is not None:
-        mesh = load_objs_as_meshes([mesh_obj_file], device=device)
-    else:
-        mesh = load_objs_as_meshes([mesh_obj_file])
-    print("Mesh loading done !!!")
-    texture_image=mesh.textures.maps_padded()
-    # plt.figure(figsize=(7,7))
-    # plt.imshow(texture_image.squeeze().cpu().numpy())
-    # plt.show()
-    RR, tt, KK, img_size_t = RtK_in_torch_format(K, RT, img_size)
-    # print(RR)
-    cameras_pytorch3d = cameras_from_opencv_projection(RR.float(), tt.float(), KK.float(), img_size_t.float())
-    if device is not None:
-        cameras_pytorch3d  = cameras_pytorch3d.to(device)
-
-    raster_settings = RasterizationSettings(
-                image_size=img_size, 
-                blur_radius=0.0, 
-                faces_per_pixel=1, 
-            )
-    # lights = lights_given_position(RT_wtoc[0:3, 3], device
-    RT = convert_w_t_c(RT)
-    if device is not None:
-        lights = lights_given_position(RT[0:3, 3], device)
-        
-    renderer = MeshRenderer(
-                rasterizer=MeshRasterizer(
-                    cameras=cameras_pytorch3d, 
-                    raster_settings=raster_settings
-                ),
-                shader=SoftPhongShader(
-                    device=device,
-                    cameras=cameras_pytorch3d,
-                    lights=lights
-                )
-            )
-    rendered_images = renderer(mesh)
-    rendered_image = rendered_images[0, ..., :3].cpu().numpy()
-
-    plt.imsave(dest_file, rendered_image)
 
 
 def viz_image(RT_list, camera, dest_dir, mesh_dir, device):
@@ -280,6 +268,7 @@ def viz_image(RT_list, camera, dest_dir, mesh_dir, device):
         os.makedirs(dest_dir)
 
     for i, RT in enumerate(RT_list):
+        print("TODO3: Lack of clarity on w_t_c. Rewrite the function as inverse_tf or something. It goes to redner_ func and gets w_to_c'ed again")
         param.extrinsic = convert_w_t_c(RT)
         dest_file = '{:02d}'.format(i) + '.png'
         dest_file = os.path.join(dest_dir, dest_file)
@@ -287,7 +276,6 @@ def viz_image(RT_list, camera, dest_dir, mesh_dir, device):
 
         # print(param.extrinsic, param.intrinsic.intrinsic_matrix, dest_file)
         redner_py3d_img(img_size, param, dest_file, mesh_dir, device)
-
 
 def viz_points_cam(centroids_coordinates, sphere_center_coords, mesh, camera, dest_dir, mesh_dir, device):
     # Visualise for particular centroid
@@ -312,7 +300,6 @@ def viz_points_cam(centroids_coordinates, sphere_center_coords, mesh, camera, de
     #print(list_of_rts)
     viz_image(list_of_rts, camera, dest_dir, mesh_dir, device)
         
-
 def synth_image(viz_pcd=False, custom_dir=False, device=None):
     #Reading data paths
     mesh_dir = "/media/shubodh/DATA/Downloads/data-non-onedrive/RIO10_data/scene01/models01/"
@@ -378,3 +365,4 @@ if __name__ == '__main__':
     viz_pcd = False
     # final_poses = poses_for_places(viz_pcd, True)
     synth_image(viz_pcd, False, device)
+
