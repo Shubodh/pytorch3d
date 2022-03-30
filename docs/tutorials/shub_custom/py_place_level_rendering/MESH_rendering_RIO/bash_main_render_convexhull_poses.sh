d3e6da070656b5f1b6b92fee4889e64557c7bbb4
Shubodh ADA g08 <p.saishubodh@gmail.com>
2022-03-30 18:31:25 +0530

    small changes: added bash file

--- docs/tutorials/shub_custom/py_place_level_rendering/MESH_rendering_RIO/bash_main_render_convexhull_poses.sh
+++ docs/tutorials/shub_custom/py_place_level_rendering/MESH_rendering_RIO/bash_main_render_convexhull_poses.sh
@@ -1,10 +1,17 @@
-echo "scene 01"
-python main_render_convexhull_poses.py --scene_id 01 --output_path scene01_rendered_images/
 echo "scene 03"
-python main_render_convexhull_poses.py --scene_id 03 --output_path scene03_rendered_images/
+python main_render_convexhull_poses.py --scene_id 03 --output_path temp_query_scene03/ --ref_or_query query
 echo "scene 05"
-python main_render_convexhull_poses.py --scene_id 05 --output_path scene05_rendered_images/
-echo "scene 07"
-python main_render_convexhull_poses.py --scene_id 07 --output_path scene07_rendered_images/
-echo "scene 09"
-python main_render_convexhull_poses.py --scene_id 09 --output_path scene09_rendered_images/
+python main_render_convexhull_poses.py --scene_id 05 --output_path temp_query_scene05/ --ref_or_query query
+
+
+#
+#echo "scene 01"
+#python main_render_convexhull_poses.py --scene_id 01 --output_path scene01_rendered_images/
+#echo "scene 03"
+#python main_render_convexhull_poses.py --scene_id 03 --output_path scene03_rendered_images/
+#echo "scene 05"
+#python main_render_convexhull_poses.py --scene_id 05 --output_path scene05_rendered_images/
+#echo "scene 07"
+#python main_render_convexhull_poses.py --scene_id 07 --output_path scene07_rendered_images/
+#echo "scene 09"
+#python main_render_convexhull_poses.py --scene_id 09 --output_path scene09_rendered_images/
